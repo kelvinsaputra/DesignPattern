c32ffc81985208ae1b2eb0554f50e9d166837016
firmanda nugroho <firmanda.nugroho@tokopedia.com>
2019-09-10 17:12:17 +0700

    Can only Scroll but Drag not working

--- bottomsheetdialog/app/src/main/java/com/example/scrollbottomsheet/FragmentBottomSheetDialog.kt
+++ bottomsheetdialog/app/src/main/java/com/example/scrollbottomsheet/FragmentBottomSheetDialog.kt
@@ -3,6 +3,7 @@ package com.example.scrollbottomsheet
 
 import android.app.Dialog
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -12,6 +13,10 @@ import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_fragment_bottom_sheet_dialog.*
+import kotlinx.android.synthetic.main.fragment_fragment_bottom_sheet_dialog.view.*
+import android.view.MotionEvent
+import android.view.View.OnTouchListener
+
 
 
 
@@ -27,6 +32,22 @@ class FragmentBottomSheetDialog : BottomSheetDialogFragment() {
 
         val view =
             inflater.inflate(R.layout.fragment_fragment_bottom_sheet_dialog, container, false)
+
+
+        view.btnAnchorTop.setOnTouchListener(OnTouchListener { v, event ->
+            // v.parent.requestDisallowInterceptTouchEvent(true)
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_UP ->  Log.d("Touching", "AnchorTop")
+            }
+            false
+        })
+        view.scroll_main.setOnTouchListener(OnTouchListener { v, event ->
+            v.parent.requestDisallowInterceptTouchEvent(true)
+            when (event.action and MotionEvent.ACTION_MASK) {
+                MotionEvent.ACTION_UP -> v.parent.requestDisallowInterceptTouchEvent(false)
+            }
+            false
+        })
         return view
     }
 
@@ -46,10 +67,16 @@ class FragmentBottomSheetDialog : BottomSheetDialogFragment() {
             parent.removeView(button)
             containerLayout.addView(button, containerLayout.childCount)
 
+
+
             val bottomSheet = dialog.findViewById<View>(R.id.design_bottom_sheet) as FrameLayout
             val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
             bottomSheetBehavior.skipCollapsed = true
             bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+
+
+            //parent.requestDisallowInterceptTouchEvent(true)
 
         }
         return bottomSheetDialog
