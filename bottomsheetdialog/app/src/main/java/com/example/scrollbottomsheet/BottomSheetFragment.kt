4e0a4eba4f75c15d3cc680d40f33618e4e98b7ef
firmanda nugroho <firmanda.nugroho@tokopedia.com>
2019-09-10 16:01:49 +0700

    Add Fully Expand and Wrap Content Expand

--- bottomsheetdialog/app/src/main/java/com/example/scrollbottomsheet/BottomSheetFragment.kt
+++ bottomsheetdialog/app/src/main/java/com/example/scrollbottomsheet/BottomSheetFragment.kt
@@ -6,8 +6,6 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-
 import kotlinx.android.synthetic.main.fragment_bottom_sheet.*
 
 
@@ -21,14 +19,13 @@ class BottomSheetFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val view = inflater.inflate(R.layout.fragment_bottom_sheet, container, false)
-
         return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val view = layoutInflater.inflate(R.layout.fragment_fragment_bottom_sheet_dialog, null)
         val bottomSheetFragment = FragmentBottomSheetDialog()
         btnShow.setOnClickListener{
             bottomSheetFragment.show(fragmentManager!!, bottomSheetFragment.tag)
--- bottomsheetdialog/app/src/main/java/com/example/scrollbottomsheet/FragmentBottomSheetDialog.kt
+++ bottomsheetdialog/app/src/main/java/com/example/scrollbottomsheet/FragmentBottomSheetDialog.kt
@@ -8,9 +8,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import kotlinx.android.synthetic.main.fragment_fragment_bottom_sheet_dialog.*
+
+
 
 /**
  * A simple [Fragment] subclass.
@@ -21,23 +24,33 @@ class FragmentBottomSheetDialog : BottomSheetDialogFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-       val view =  inflater.inflate(R.layout.fragment_fragment_bottom_sheet_dialog, container, false)
+
+        val view =
+            inflater.inflate(R.layout.fragment_fragment_bottom_sheet_dialog, container, false)
         return view
     }
 
+
+
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+
         val bottomSheetDialog = super.onCreateDialog(savedInstanceState) as BottomSheetDialog
         bottomSheetDialog.setOnShowListener {
             val dialog = it as BottomSheetDialog
-
             dialog.findViewById<FrameLayout>(com.google.android.material.R.id.design_bottom_sheet) as FrameLayout
             val containerLayout: FrameLayout =
                 dialog.findViewById<FrameLayout>(com.google.android.material.R.id.container) as FrameLayout
+
             val button = btnAnchor
             val parent = button.parent as ViewGroup
             parent.removeView(button)
             containerLayout.addView(button, containerLayout.childCount)
+
+            val bottomSheet = dialog.findViewById<View>(R.id.design_bottom_sheet) as FrameLayout
+            val bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet)
+            bottomSheetBehavior.skipCollapsed = true
+            bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
         }
         return bottomSheetDialog
     }
