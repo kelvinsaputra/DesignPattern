3a3631ce764aa7e06b972da0c022deec938876dd
leonard2907 <leonard.gunawan@tokopedia.com>
2019-09-12 14:12:30 +0700

    Added Cardview to XML

--- CustomComponent/app/src/main/java/com/example/customcomponent/CustomComponent.kt
+++ CustomComponent/app/src/main/java/com/example/customcomponent/CustomComponent.kt
@@ -22,11 +22,6 @@ class CustomComponent @JvmOverloads constructor(
 
         attrs?.let {
             val typedArray = context.obtainStyledAttributes(it, R.styleable.custom_component_attributes, 0, 0)
-            val title = resources.getText(typedArray
-                .getResourceId(R.styleable.custom_component_attributes_custom_component_title, R.string.component_one))
-
-            my_title.text = title
-            my_edit.hint = "${resources.getString(R.string.hint_text)} $title"
 
             typedArray.recycle()
         }
