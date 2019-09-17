59b72b553c20f6511a8d62b1e877d4a663654a22
Kelvin Saputra <nakama@202729.local>
2019-09-17 12:07:11 +0700

    tried basic coroutine

--- androidFundamentals/Kotlin/app/src/main/java/com/example/kotlin/Coroutine/CoroutineActivity.kt
+++ androidFundamentals/Kotlin/app/src/main/java/com/example/kotlin/Coroutine/CoroutineActivity.kt
@@ -1,11 +1,17 @@
 package com.example.kotlin.Coroutine
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import com.example.kotlin.R
 
 import kotlinx.android.synthetic.main.activity_coroutine.*
+import kotlinx.android.synthetic.main.content_coroutine.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class CoroutineActivity : AppCompatActivity() {
 
@@ -13,6 +19,24 @@ class CoroutineActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_coroutine)
         setSupportActionBar(toolbar)
+
+        GlobalScope.launch {
+            text1.text = "Coroutine works"
+            delay(3000)
+            Log.d("Hello", "HELLLLOOOOOOO")
+        }
+
+//        GlobalScope.launch(Dispatchers.Main) { // launch coroutine in the main thread
+//            for (i in 10 downTo 1) { // countdown from 10 to 1
+//                text1.text = "Countdown $i ..." // update text
+//                delay(500) // wait half a second
+//            }
+//            text1.text = "Done!"
+//        }
+
+        Thread.sleep(1000)
+        Log.d("Hello", "WOOOOORRRRRLLLLLDDDDD")
+        text2.text = "Thread sleep here"
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
