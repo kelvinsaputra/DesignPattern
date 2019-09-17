b14df2483697878b8507f12446d5de405c4d53dc
Kelvin Saputra <nakama@202729.local>
2019-09-17 14:04:07 +0700

    Test Observer Pattern

--- androidFundamentals/designPattern/app/src/main/java/com/example/designpattern/MainActivity.kt
+++ androidFundamentals/designPattern/app/src/main/java/com/example/designpattern/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.example.designpattern
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.designpattern.behavioralPattern.ObserverPattern.ObserverActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        observer.setOnClickListener {
+            val intent = Intent(this@MainActivity, ObserverActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ androidFundamentals/designPattern/app/src/main/java/com/example/designpattern/behavioralPattern/ObserverPattern/KrabbyPatty.kt
@@ -0,0 +1,7 @@
+package com.example.designpattern.behavioralPattern.ObserverPattern
+
+class KrabbyPatty (
+    var name: String,
+    var price: Int,
+    var pickle: Boolean
+)
